a168351496df19d9f67f299d295756802f538bfd
Zak Fargo <zfargo@microsoft.com>
2018-02-14 11:43:03 -0800

    Fixing build script have to push file

--- resume.js
+++ resume.js
@@ -2,4 +2,4 @@ function openRawFile(drilldown)
 {
     appInsights.trackEvent("drilldown", { page = drilldown });
     window.open("/resume/" + drilldown + "?commit=<!-- COMMITHERE -->", "_blank");
-} 
+}
