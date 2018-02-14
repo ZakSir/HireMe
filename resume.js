9c253d29c638494b1cee780f4864b051ac426668
Zak Fargo <zfargo@microsoft.com>
2018-02-14 12:11:03 -0800

    Adding linking for github

--- resume.js
+++ resume.js
@@ -3,3 +3,9 @@ function openRawFile(drilldown)
     appInsights.trackEvent("drilldown", { page: drilldown, commit: '<!-- COMMITHERE -->' });
     window.open("/resume/" + drilldown + "?commit=<!-- COMMITHERE -->", "_blank"); 
 }
+
+function openGitHubCommit()
+{
+    appInsights.trackEvent("githubcommit", { commit: '<!-- COMMITHERE -->' });
+    window.open("https://github.com/ZakSir/HireMe/commit/<!-- COMMITHERE -->", "_blank");
+}
